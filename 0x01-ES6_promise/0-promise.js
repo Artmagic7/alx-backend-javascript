165f82bd0ef92e6426c2e5f4b4b82a074cbdaa13
Arthur Owens <arth.owens@gmail.com>
2024-04-09 19:46:02 +0000

    ES6 Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Hello world!');
-    }, 300);
-  });
+  return new Promise(() => {});
 }
