6dacc915b700a801027b6c47055de110375b8c22
Arthur Owens <arth.owens@gmail.com>
2024-06-04 20:58:53 +0000

    0x05. NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displaying message on STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
